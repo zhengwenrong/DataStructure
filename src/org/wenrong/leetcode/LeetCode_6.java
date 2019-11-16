92074e72cabd77358937d9b3ed8da8857c9e655d
wenrong <15018159367@163.com>
2019-11-16 22:10:11 +0800

    实现环形单向链表 2019年11月16日22:08:08

---
+++ src/org/wenrong/leetcode/LeetCode_6.java
@@ -0,0 +1,25 @@
+package org.wenrong.leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LeetCode_6 {
+
+    public String convert(String s, int numRows) {
+
+        char[][] chars = new char[3][1];
+
+
+
+
+        return "";
+    }
+
+    public static void main(String[] args) throws IOException {
+
+
+
+
+    }
+}
--- src/org/wenrong/linked/CurcleLinkedList.java
+++ src/org/wenrong/linked/CurcleLinkedList.java
@@ -5,36 +5,114 @@ package org.wenrong.linked;
  */
 public class CurcleLinkedList {
 
-    private Node headNode;
+    private Node headNode = new Node();
+
+    private Node currentNode;
+
+    private void add(int data){
+
+        Node lastNode = lastNode();
+        Node node = new Node(data);
+        lastNode.next = node;
+        node.next = headNode;
 
 
+    }
+
+    @Override
+    public String toString() {
+
+        Node node = headNode.next;
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("[");
+
+        while(node != null && node.data != null){
+
+            buffer.append(node.data);
+            buffer.append(",");
+            node = node.next;
+        }
+        buffer.append("]");
+        String string = buffer.toString();
 
 
+        return string.substring(0,string.lastIndexOf(",")) + "]";
+    }
 
+    private Node lastNode(){
 
+        Node lastNode = headNode;
 
+        while (lastNode.next != null && lastNode.next.data != null) {
+            lastNode = lastNode.next;
+        }
 
+        return lastNode;
+    }
 
+    private Node getNodeAt(int index){
 
+        Node node = headNode;
 
+        int i = -1;
+
+        while (node.next!=null && node.next.data != null) {
+
+            if(i == index){
+
+                return node;
+            }
+
+            node = node.next;
+            i++;
+        }
+
+        return null;
+    }
+
+    private int remove(int index){
+
+        Node nodeAt = getNodeAt(index);
+
+        return 0;
+    }
 
     public static void main(String[] args) {
 
+        CurcleLinkedList list = new CurcleLinkedList();
+
+        for(int i = 0 ; i < 1000; i++){
+
+            list.add(i);
+
+        }
+
+        System.out.println(list.getNodeAt(0));
     }
 
     class Node {
 
-        public org.wenrong.linked.Node next;
-        public int data;
+        public Node next;
+        public Integer data;
 
         public Node(){}
 
-        public Node(org.wenrong.linked.Node next, int data){
+        public Node(int data){
+
+            this.data= data;
+
+        }
+
+        public Node(Node next, int data){
             this.next = next;
             this.data = data;
         }
 
+        @Override
+        public String toString() {
+            return this.data+ "";
+        }
     }
-
 
 }
