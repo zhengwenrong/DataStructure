a33c5f5806a5fc300ff87410ca689414cb1684b1
wenrong <15018159367@163.com>
2019-11-17 17:03:47 +0800

    Leetcode 第六题 2019年11月17日17:03:33

--- src/org/wenrong/leetcode/LeetCode_6.java
+++ src/org/wenrong/leetcode/LeetCode_6.java
@@ -3,30 +3,66 @@ package org.wenrong.leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class LeetCode_6 {
 
-    public String convert(String s, int numRows) {
+    public static String convert(String s, int numRows) {
+
+        if(s.length() == 1 || numRows == 1)
+            return s;
 
         int size = 2 * numRows - 2 ;
 
+        List<Character> listArr[] = new ArrayList[numRows];
 
+        for(int i = 0; i < numRows;i++)
+            listArr[i] = new ArrayList();
 
+        int direction = -1;
 
-//        for(){
-//
-//
-//
-//
-//        }
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        for(int i = 0 ; i < length ;i++){
 
-        return "";
+            if(i!= 0 &&i % size == 0)
+                direction = -1;
+
+            if(i % size >=  numRows ){
+
+                direction--;
+                listArr[direction].add(chars[i]);
+
+            }else {
+                direction++;
+                listArr[direction].add(chars[i]);
+
+            }
+
+        }
+
+        String ss = "";
+
+        for(List list:listArr){
+
+            for(Object c : list){
+
+                ss += c;
+
+            }
+        }
+
+        return ss;
     }
 
 
     public static void main(String[] args) throws IOException {
 
+        String s = "abc";
+
+        System.out.println(convert(s,2));
 
 
 
